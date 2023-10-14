a33a9d8e9ead3a2d21eab524dab176ed35718317
niraj-rayalla <niraj.rayalla@gmail.com>
2023-10-14 13:48:54 -0500

    Initial commit for GDXseer which is Effekseer for use in libGDX.

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        val kotlinVersion: String by settings
+        id("org.jetbrains.kotlin.jvm") version "1.9.0"
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GDXseer"
+ 
