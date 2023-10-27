3ce71c112a941227662807ec0513340c22ae9832
niraj-rayalla <niraj.rayalla@gmail.com>
2023-10-27 13:48:45 -0500

    Updated EffekseerMetalManagerAdapter.

--- cpp/EffekseerAdapters/Metal/EffekseerMetalManagerAdapter.h
+++ cpp/EffekseerAdapters/Metal/EffekseerMetalManagerAdapter.h
@@ -2,15 +2,47 @@
 
 #include "../EffekseerManagerAdapter.h"
 #include "../../Effekseer/Dev/Cpp/EffekseerRendererMetal/EffekseerRendererMetal.h"
+#include "../EffekseerManagerAdapter.h"
+#include <LLGI.Base.h>
+#include <LLGI.Compiler.h>
+#include <LLGI.Graphics.h>
+#include <LLGI.Platform.h>
+#include <LLGI.GraphicsMetal.h>
+#include <LLGI.CommandListMetal.h>
+#include <LLGI.TextureMetal.h>
+#include <LLGI.RenderPassMetal.h>
+#include <GraphicsDevice.h>
+#include <Utils/LLGI.CommandListPool.h>
 
 class EffekseerMetalManagerAdapter : public EffekseerManagerAdapter {
 
 protected:
-    Effekseer::RefPtr<EffekseerRenderer::SingleFrameMemoryPool> memoryPool;
-    Effekseer::RefPtr<EffekseerRenderer::CommandList> commandList;
+    LLGI::GraphicsMetal* graphics;
+    LLGI::TextureMetal* texture;
+    std::shared_ptr<LLGI::SingleFrameMemoryPool> memoryPool;
+    std::shared_ptr<LLGI::CommandListPool> commandListPool;
+    LLGI::CommandList* commandList = nullptr;
+
+    Effekseer::Backend::GraphicsDeviceRef efkGraphicsDevice;
+    EffekseerRenderer::RendererRef efkRenderer;
+    Effekseer::RefPtr<EffekseerRenderer::SingleFrameMemoryPool> efkMemoryPool;
+    Effekseer::RefPtr<EffekseerRenderer::CommandList> efkCommandList;
 
     EffekseerRendererRefWrapper CreateRenderer(int32_t spriteMaxCount) override;
 
+    id<MTLRenderCommandEncoder> GetEncoder()
+    {
+        return static_cast<LLGI::CommandListMetal*>(commandList)->GetRenderCommandEncorder();
+    }
+
 public:
+    id<CAMetalDrawable>* currentDrawable = nullptr;
+
     EffekseerMetalManagerAdapter(int32_t spriteMaxCount, bool autoFlip = true);
+    ~EffekseerMetalManagerAdapter();
+
+    void StartFrame();
+    void EndFrame();
+
+    void DrawCombined(const Effekseer::Manager::DrawParameter& drawParameter);
 };
--- cpp/EffekseerAdapters/Metal/EffekseerMetalManagerAdapter.mm
+++ cpp/EffekseerAdapters/Metal/EffekseerMetalManagerAdapter.mm
@@ -2,11 +2,71 @@
 
 EffekseerMetalManagerAdapter::EffekseerMetalManagerAdapter(int32_t spriteMaxCount, bool autoFlip): EffekseerManagerAdapter(spriteMaxCount, autoFlip) { }
 
-EffekseerRendererRefWrapper EffekseerMetalManagerAdapter::CreateRenderer(int32_t spriteMaxCount) {
-    EffekseerRenderer::RendererRef rendererRef = EffekseerRendererMetal::Create(spriteMaxCount, MTLPixelFormatBGRA8Unorm, MTLPixelFormatInvalid, false);
+EffekseerMetalManagerAdapter::~EffekseerMetalManagerAdapter() {
+    delete graphics;
+    delete texture;
+}
 
-    memoryPool = EffekseerRenderer::CreateSingleFrameMemoryPool(rendererRef->GetGraphicsDevice());
-    commandList = EffekseerRenderer::CreateCommandList(rendererRef->GetGraphicsDevice(), memoryPool);
+EffekseerRendererRefWrapper EffekseerMetalManagerAdapter::CreateRenderer(int32_t spriteMaxCount) {
+    graphics = new LLGI::GraphicsMetal();
+    graphics->Initialize(nullptr);
+    efkGraphicsDevice = Effekseer::MakeRefPtr<EffekseerRendererLLGI::Backend::GraphicsDevice>(graphics);
+
+    memoryPool = LLGI::CreateSharedPtr(graphics->CreateSingleFrameMemoryPool(1024 * 1024, 128));
+    commandListPool = std::make_shared<LLGI::CommandListPool>(graphics, memoryPool.get(), 3);
+
+    EffekseerRenderer::RendererRef rendererRef = EffekseerRendererMetal::Create(efkGraphicsDevice, spriteMaxCount, MTLPixelFormatBGRA8Unorm, MTLPixelFormatInvalid, false);
+    efkRenderer = rendererRef;
+
+    efkMemoryPool = EffekseerRenderer::CreateSingleFrameMemoryPool(rendererRef->GetGraphicsDevice());
+    efkCommandList = EffekseerRenderer::CreateCommandList(rendererRef->GetGraphicsDevice(), efkMemoryPool);
+
+    texture = new LLGI::TextureMetal();
+    currentDrawable = nullptr;
 
     return EffekseerRendererRefWrapper(rendererRef);
+}
+
+void EffekseerMetalManagerAdapter::StartFrame() {
+    //if (currentDrawable != nullptr) {
+        memoryPool->NewFrame();
+        commandList = commandListPool->Get();
+
+        commandList->Begin();
+        // Get the render pass
+        LLGI::RenderPassMetal* renderPass = graphics->GetRenderPass();
+        /*
+        texture->Reset(currentDrawable->texture);
+        renderPass->UpdateRenderTarget((LLGI::Texture**)&texture, 1, nullptr, nullptr, nullptr);
+        renderPass->SetIsColorCleared(false);
+        renderPass->SetIsDepthCleared(false);
+         */
+        // Begin the render pass
+        commandList->BeginRenderPass(static_cast<LLGI::RenderPass*>(renderPass));
+
+        efkMemoryPool->NewFrame();
+
+        EffekseerRendererMetal::BeginCommandList(efkCommandList, GetEncoder());
+        efkRenderer->SetCommandList(efkCommandList);
+    //}
+}
+
+void EffekseerMetalManagerAdapter::EndFrame() {
+    //if (currentDrawable != nullptr) {
+        efkRenderer->SetCommandList(nullptr);
+        EffekseerRendererMetal::EndCommandList(efkCommandList);
+
+        commandList->EndRenderPass();
+        commandList->End();
+
+        graphics->Execute(commandList);
+
+        //currentDrawable = nullptr;
+    //}
+}
+
+void EffekseerMetalManagerAdapter::DrawCombined(const Effekseer::Manager::DrawParameter& drawParameter) {
+    this->StartFrame();
+    EffekseerManagerAdapter::DrawCombined(drawParameter);
+    this->EndFrame();
 }
